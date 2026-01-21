a01741a77646303bb201a7a96be3d6ddb6ddd373
remixan07 <remixan07@gmail.com>
2026-01-21 07:27:56 -0600

    Actividad 2 - Versión 1 - 21/01/26 - 07:26

---
+++ POO_Ejercicios_Semana_2.java
@@ -0,0 +1,12 @@
+package Actividad 2;
+
+public class POO_Ejercicios_Semana_2 {
+    String name = "Aquiles Esquivel Madrazo";
+    Scanner scanner = new System (Scanner.in);
+    System.out.println("Bienvenid@");
+    System.out.println("Por favor, ingrese su nombre");
+    String inserted_name;
+    if name = Ricardo
+    System.out.println("Hola Hola, Pirinola");
+
+}
