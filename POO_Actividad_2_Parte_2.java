b534d6df01114fde65bc6dc5eb3e4ff2c88626aa
remixan07 <remixan07@gmail.com>
2026-01-22 12:46:28 -0600

    Actividad 2 - Parte 2 - Versión 1 - 22/01/26 - 12:46

---
+++ POO_Actividad_2_Parte_2.java
@@ -0,0 +1,11 @@
+public class POO_Actividad_2_Parte_2 {
+        public static void main(String[] args) {
+            int [] num = newint[50]
+            int = 0;
+            for i in range = 2 i <= 100:
+            if (i % 2 == 0) {
+                
+            }
+    
+}
+}
