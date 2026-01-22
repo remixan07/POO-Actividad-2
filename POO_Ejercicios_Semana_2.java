c46a70cbd890f1163aa3c425f005c9459f46f7cc
remixan07 <remixan07@gmail.com>
2026-01-22 08:08:16 -0600

    Actividad 2 - Versión 4 - 22/01/26 - 08:08

--- POO_Ejercicios_Semana_2.java
+++ POO_Ejercicios_Semana_2.java
@@ -4,16 +4,18 @@ public class POO_Ejercicios_Semana_2 {
         Scanner sc = new Scanner (System.in);
         String name = "Aquiles Esquivel Madrazo";
         System.out.println("Bienvenid@");
-        System.out.println("Por favor, ingrese su nombre ");
+        System.out.print("Por favor, ingrese su nombre ");
         String inserted_name = sc.nextLine();
-
-    if (inserted_name == name) {
-        System.out.println("Hola Hola, Pirinola");
-        System.out.println("¿Cómo te va", name, "?");
-    }
-    else {
-        System.out.println("Amiguito, no estás registrado");
-        System.out.println("Intenta con otro nombre");
+        
+        if (inserted_name == ignoreCase(name)) {
+            System.out.println("Hola Hola, Pirinola");
+            System.out.println("¿Cómo le va" + name +"?");
         }
+        
+        else {
+            System.out.println("Amiguito, no estás registrado");
+            System.out.println("el nombre" + inserted_name + "no se encuentra en el sistema");
+        }
+        sc.close();
     }
 }
