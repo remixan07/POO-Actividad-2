375f480fc204032008dfbfdcfa1d4d278e190940
remixan07 <remixan07@gmail.com>
2026-01-22 11:45:43 -0600

    Actividad 2 - Versión 5 - 22/01/26 - 11:45

--- POO_Ejercicios_Semana_2.java
+++ POO_Ejercicios_Semana_2.java
@@ -1,21 +1,26 @@
+
 import java.util.Scanner;
+
 public class POO_Ejercicios_Semana_2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         String name = "Aquiles Esquivel Madrazo";
         System.out.println("Bienvenid@");
-        System.out.print("Por favor, ingrese su nombre ");
+        System.out.printin("Por favor, ingrese su nombre ");
         String inserted_name = sc.nextLine();
-        
-        if (inserted_name == ignoreCase(name)) {
-            System.out.println("Hola Hola, Pirinola");
+
+        if (inserted_name.equalsIgnoreCase(name)) {
+            System.err.println("Hola Hola, Pirinola");
             System.out.println("¿Cómo le va" + name +"?");
+            System.out.println("Usted cuenta con acceso al Sistema :)");
+        } else if (inserted_name.trim().isEmpty()) {
+            System.out.println("El nombre no es válido :|");
+        } else {
+            System.out.println("Amiguito")
+            System.out.println("El nombre " + inserted_name + " no se encuentra en el sistema :(");
+            System.out.println("Para la próxima, elija un nombre que aplique");
         }
-        
-        else {
-            System.out.println("Amiguito, no estás registrado");
-            System.out.println("el nombre" + inserted_name + "no se encuentra en el sistema");
-        }
+
         sc.close();
     }
 }
