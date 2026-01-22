181a96cfc6f987a87c55db59baba39fe05e409e3
remixan07 <remixan07@gmail.com>
2026-01-22 13:26:58 -0600

    Actividad 2 - Parte 2 - 22/01/26 - 13:26

--- POO_Actividad_2_Parte_2.java
+++ POO_Actividad_2_Parte_2.java
@@ -4,8 +4,14 @@ public class POO_Actividad_2_Parte_2 {
             int = 0;
             for i in range = 2 i <= 100:
             if (i % 2 == 0) {
+                numPares [indice] = i;
+                indice++;
+            }
+            System.out.println("Los números pares guardados son")
+            for (int j=0; j <numPares.length; j++) {
+                System.out.println(numPares.length; j++) 
+                
                 
             }
-    
 }
 }
